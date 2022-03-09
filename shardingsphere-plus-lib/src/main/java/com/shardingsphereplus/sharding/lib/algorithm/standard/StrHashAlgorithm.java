b35375bef30e48884050ccb1d6b88d50579b82a7
Jobs <wchswchs@gmail.com>
2022-03-09 23:13:23 +0800

    update

---
+++ shardingsphere-plus-lib/src/main/java/com/shardingsphereplus/sharding/lib/algorithm/standard/StrHashAlgorithm.java
@@ -0,0 +1,82 @@
+package com.shardingsphereplus.sharding.lib.algorithm.standard;
+
+import com.google.common.hash.Hashing;
+import com.shardingsphereplus.sharding.lib.algorithm.AbstractStandardShardingAlgorithm;
+import com.shardingsphereplus.sharding.lib.utils.StringUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
+import org.apache.shardingsphere.underlying.common.exception.ShardingSphereException;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+
+@Slf4j
+public class StrHashAlgorithm extends AbstractStandardShardingAlgorithm<String> {
+
+    private int startIndex = -1;
+    private int endIndex = -1;
+
+    public StrHashAlgorithm() {
+    }
+
+    public StrHashAlgorithm(int startIndex, int endIndex, String partitionJoinDelimiter) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        if (StringUtils.isNotEmpty(partitionJoinDelimiter)) {
+            this.partitionJoinDelimiter = partitionJoinDelimiter;
+        }
+    }
+
+    @Override
+    public String doSharding(Collection<String> partitions, PreciseShardingValue<String> shardingValue) {
+        if (CollectionUtils.isEmpty(partitions)
+                || shardingValue == null
+                || StringUtils.isEmpty(shardingValue.getLogicTableName())
+                || shardingValue.getValue() == null) {
+            throw new IllegalArgumentException("sharding partitions or key can not be empty");
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("sharding params [partitions={}, logicTable={}, columnName={}, columnValue={}]",
+                    ArrayUtils.toString(partitions),
+                    shardingValue.getLogicTableName(),
+                    shardingValue.getColumnName(),
+                    shardingValue.getValue()
+                    );
+        }
+        String shardingColumnValue = StringUtil.subString(shardingValue.getValue(), startIndex, endIndex);
+        //use murmurhash3 because it's collision is lower than hashCode()
+        int hash = Math.abs(Hashing.murmur3_128(0x1234ABCD).newHasher()
+                .putString(shardingColumnValue, StandardCharsets.UTF_8)
+                .hash().asInt());
+        int index = hash % partitions.size();
+        String routedPartition = StringUtils.leftPad(String.valueOf(index), 3, '0');
+        if (log.isDebugEnabled()) {
+            log.debug("compute hashcode and routed partition, [hashcode={}, index={}, partition={}]",
+                    hash, index, routedPartition);
+        }
+        if (!partitions.contains(routedPartition)) {
+            throw new ShardingSphereException("unavailable routed partition: " + routedPartition);
+        }
+        return StringUtils.joinWith(partitionJoinDelimiter, shardingValue.getLogicTableName(), routedPartition);
+    }
+
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public int getEndIndex() {
+        return endIndex;
+    }
+
+    public void setEndIndex(int endIndex) {
+        this.endIndex = endIndex;
+    }
+
+}
---
+++ shardingsphere-plus-lib/src/test/java/com/shardingsphereplus/sharding/lib/algorithm/StrHashAlgorithmTest.java
@@ -0,0 +1,63 @@
+package com.shardingsphereplus.sharding.lib.algorithm;
+
+import com.shardingsphereplus.sharding.lib.algorithm.standard.StrHashAlgorithm;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StrHashAlgorithmTest {
+
+    private final String logicTable = "table";
+    private final List<String> partitions = new ArrayList<>();
+    private PreciseShardingValue<String> preciseShardingValue;
+
+    private final StrHashAlgorithm strHashAlgorithm = new StrHashAlgorithm();
+
+    @Test
+    @DisplayName("截取子串分片测试")
+    public void testDoSharding4Substring() {
+        strHashAlgorithm.setEndIndex(2);
+        Assertions.assertEquals(
+                StringUtils.joinWith("_", logicTable, partitions.get(11)),
+                strHashAlgorithm.doSharding(partitions, preciseShardingValue)
+        );
+    }
+
+    @Test
+    @DisplayName("整串分片测试")
+    public void testDoSharding() {
+        Assertions.assertEquals(
+                StringUtils.joinWith("_", logicTable, partitions.get(15)),
+                strHashAlgorithm.doSharding(partitions, preciseShardingValue)
+        );
+    }
+
+    @Test
+    @DisplayName("空分片字段异常测试")
+    public void testEmptyException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            strHashAlgorithm.doSharding(partitions, (PreciseShardingValue<String>) null);
+        });
+    }
+
+    @BeforeEach
+    public void setUp() {
+        for (int i = 0; i < 32; i ++) {
+            String partition = StringUtils.leftPad(String.valueOf(i), 3, '0');
+            partitions.add(partition);
+        }
+
+        String columnName = "column1";
+        String shardingValue = "S48c1jogpnd3";
+        preciseShardingValue =
+                new PreciseShardingValue<>(logicTable, columnName, shardingValue);
+
+    }
+
+}
